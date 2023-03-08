e7fbdef52b9dfde8b82766a24401eaf7456d6f5c
SCollins <sje.collins@gmail.com>
2023-03-08 14:17:54 +0000

    start nesting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
+import ConditionalRenderingClass from './components/ConditionalRenderingClass';
+import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
 import EventsClass from './components/EventsClass';
 import EventsFunctional from './components/EventsFunctional';
+import NestingComponents from './components/NestingComponents';
 
 function App() {
   return (
     <div className="App">
       <EventsClass />
       <EventsFunctional />
+      <ConditionalRenderingFunctional connected={true} />
+      <ConditionalRenderingClass />
+      <NestingComponents />
     </div>
   );
 }
